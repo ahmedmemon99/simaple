9f9a567d5316947e7a08b333136796e699c5438e
dasdddd <ahmed@ki.com>
2023-04-17 17:22:14 +0530

    initial commit

---
+++ project/Flutter_ECom_App/lib/main.dart
@@ -0,0 +1,13 @@
+void main(){
+ runApp(app) 
+}
+
+
+class FlutterECom extends StatelessWidget {
+  const FlutterECom({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
